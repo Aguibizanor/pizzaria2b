7ab0ef0457279f9cbbe7715671d8266333aa7ee4
Aguibizanor <aguibizanor@gmail.com>
2024-06-11 08:29:44 -0300

    Uns Coiso Sapo

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import {useState} from 'react'
 import { Routes, Route} from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Inicio from './' /*Colocar este caminho depois*/
+import Inicio from './pages/Inicio'
 
 const App = () => {
 
-  const [color] = useState("#eb8d04")
+  const [color] = useState("#836FFF")
   document.body.style.backgroundColor = color
 
   return (
@@ -14,6 +14,7 @@ const App = () => {
       <div>
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path="/inicio" element={<Inicio/>} />
         </Routes>
       </div>
     </div>
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -6,8 +6,15 @@ const Home = () => {
 
     return (
         <div>
-            <h2>Izonça Sapin</h2>
-           <p>Outra tela Inicial</p>
+            <h1>Izonça Sapin</h1>
+           <p><h2>O horror</h2><br/>
+            Pela necessidade de amar, todos morreriam<br/>
+            O amor está morrendo, pelo último vestígio de fé<br/>
+            O céu cai diante de meus olhos, infurece meus sentidos<br/>
+            Ocorrendo a extrapolação da vida<br/>
+            Quebra minhas pontes e arrasa minhas lágrimas<br/>
+            Condensando-as em um lago, onde me afogo.<br/>
+           </p>
                 <img className='img-fluid' src={logo}
                         alt='logo' />
            
